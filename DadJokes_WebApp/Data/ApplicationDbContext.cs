c88f901c98e555de440b5ceedf52d4ff28e97bad
Malik El Guermai <malikelguermai@outlook.com>
2022-05-15 13:31:50 -0400

    Initial Commit

---
+++ DadJokes_WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DadJokes_WebApp.Models;
+
+namespace DadJokes_WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Joke>? Joke { get; set; }
+    }
+}
---
+++ DadJokes_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DadJokes_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
